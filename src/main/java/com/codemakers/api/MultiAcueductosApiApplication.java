aecb2e31dccc4f1312813874665b86bb03e6b86d
DESKTOP-DL3TM1L\ADMIN <nicope2004@gmail.com>
2025-05-27 11:30:12 -0500

    first commit

---
+++ src/main/java/com/codemakers/api/MultiAcueductosApiApplication.java
@@ -0,0 +1,13 @@
+package com.codemakers.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAcueductosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiAcueductosApiApplication.class, args);
+	}
+
+}
